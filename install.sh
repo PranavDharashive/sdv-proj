ffa2149be9c6ebd3ff8abb6fc6ff4f5eaaca23b4
pranavdharashive <pranavdharashive@gmail.com>
2025-07-25 19:18:49 +0530

    again changed kube file path

--- install.sh
+++ install.sh
@@ -2,5 +2,8 @@
 
 LOG_FILE="install-$(date +%Y%m%d-%H%M%S).log"
 
+# Set KUBECONFIG environment variable for Ansible
+export KUBECONFIG="$HOME/.kube/config"
+
 # Run the ansible playbook and log the output
 ansible-playbook -i inventory.ini ansible/install.yml | tee $LOG_FILE
