9fa3879be6ac3fbe85fcb43de54718cef017c316
pranavdharashive <pranavdharashive@gmail.com>
2025-07-30 17:18:12 +0530

    ip input and tain skip fixed

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@
 
 LOG_FILE="install-$(date +%Y%m%d-%H%M%S).log"
 
+# Prompt for Kube-apiserver endpoint IP
+read -p "Enter the private IP address for Kube-apiserver endpoint: " KUBE_API_IP
+
 # Set KUBECONFIG environment variable for Ansible
 export KUBECONFIG="$HOME/.kube/config"
 
 # Run the ansible playbook and log the output
-ansible-playbook -i inventory.ini ansible/install.yml | tee $LOG_FILE
+ansible-playbook -i inventory.ini ansible/install.yml -e "kube_api_ip=$KUBE_API_IP" | tee $LOG_FILE
