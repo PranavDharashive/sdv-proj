eb2162e280d44f459d3bb7aec0f5b7a46122c718
pranavdharashive <pranavdharashive@gmail.com>
2025-09-09 14:24:50 +0530

    k8s version input updated

--- install.sh
+++ install.sh
@@ -5,11 +5,14 @@ LOG_FILE="install-$(date +%Y%m%d-%H%M%S).log"
 # Prompt for Kube-apiserver endpoint IP
 read -p "Enter the private IP address for Kube-apiserver endpoint: " KUBE_API_IP
 
+# Prompt for Kubernetes version
+read -p "Enter the Kubernetes version to install (e.g., 1.31.0): " K8S_VERSION
+
 # Set KUBECONFIG environment variable for Ansible
 export KUBECONFIG="$HOME/.kube/config"
 
 # Run the ansible playbook and log the output
-ansible-playbook -i inventory.ini ansible/install.yml -e "kube_api_ip=$KUBE_API_IP" | tee $LOG_FILE
+ansible-playbook -i inventory.ini ansible/install.yml -e "kube_api_ip=$KUBE_API_IP" -e "k8s_version=$K8S_VERSION" | tee $LOG_FILE
 
 # Check if the ansible-playbook command was successful
 # if [ $? -eq 0 ]; then
